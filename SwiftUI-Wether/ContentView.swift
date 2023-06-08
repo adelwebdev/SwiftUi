5ad29563aac9995b90051505d29f53f08d48c666
ADEL BOUATTA <83227627+adelwebdev@users.noreply.github.com>
2023-06-08 20:23:18 +0300

    Add files via upload

---
+++ SwiftUI-Wether/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  SwiftUI-Wether
+//
+//  Created by BOUATTA ADEL on 08.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint:.topLeading,
+                endPoint: .bottomTrailing)
+            .edgesIgnoringSafeArea(.all)
+            VStack{
+                Text("Cupertino, CA")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                VStack(spacing: 10){
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    
+                    Text("76")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                        
+                }
+                
+                HStack{
+                    
+                }
+                Spacer()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Wether/SwiftUI_WetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WetherApp.swift
+//  SwiftUI-Wether
+//
+//  Created by BOUATTA ADEL on 08.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
