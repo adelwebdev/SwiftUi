dee8a8c3a7fb3bcbee4a0c01ea0913138ff1dcf1
ADEL BOUATTA <83227627+adelwebdev@users.noreply.github.com>
2023-06-12 21:03:24 +0300

    Add files via upload

--- SwiftUI-Wether/ContentView.swift
+++ SwiftUI-Wether/ContentView.swift
@@ -8,13 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isNight = false
+    
     var body: some View {
         ZStack {
-            BackgroundView(topColor: .blue, bottomColor: Color("myColor"))
+            BackgroundView(isNight: $isNight)
             VStack{
                 CityTextView(cityName: "Cupertino, CA")
                
-                MainWeatherStatusView(imageName: "cloud.sun.fill", temperature: 74)
+                MainWeatherStatusView(imageName: isNight ? "moon.stars.fill" : "cloud.sun.fill", temperature: 74)
                 
                 HStack{
                     WeatherDayView(dayOfWeek: "TUE",
@@ -36,7 +39,7 @@ struct ContentView: View {
                 Spacer()
                 
                 Button{
-                    print("tapped")
+                    isNight.toggle()
                 } label: {
                     WeatherButton(title: "Change Day Time", textColor: .blue,
                                   backgroundColor: .white)
@@ -83,12 +86,11 @@ struct WeatherDayView: View {
 
 struct BackgroundView: View {
     
-    var topColor: Color
-    var bottomColor: Color
+    @Binding var isNight : Bool
     
     var body: some View {
 
-        LinearGradient(gradient: Gradient(colors: [topColor, bottomColor]),
+        LinearGradient(gradient: Gradient(colors: [isNight ? .black : .blue, isNight ? .gray : Color("myColor")]),
                        startPoint:.topLeading,
                        endPoint: .bottomTrailing)
         .edgesIgnoringSafeArea(.all)
